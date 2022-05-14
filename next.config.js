2cbf4e11b81054280f6f224dc09384a0353b0784
Kien Ngo <kienn@aareas.com>
2022-05-14 02:18:22 -0500

    Init nextjs + tailwindcss

---
+++ next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
+
+module.exports = {
+  env: {
+    blockFrostApiKey: 'ipfs3sx6rgPOcEf2DLfFNWJ9Wp1UvmllV2dV'
+  }
+}
+
+module.exports = {
+  images: {
+    domains: [
+      'https://img.freepik.com/free-vector/image-upload-concept-landing-page_52683-27130.jpg'
+    ]
+  },
+}
